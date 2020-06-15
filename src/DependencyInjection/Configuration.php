51c9a46ff2d8cbad8b29f19aa07b6389e5ece0c9
Niels Janssen <nielsjanssen@live.nl>
2020-06-15 16:53:56 +0200

    Fix Symfony 5 compatibility

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -5,11 +5,6 @@ namespace Riverline\WorkerBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-/**
- * This is the class that validates and merges configuration from your app/config files
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
- */
 class Configuration implements ConfigurationInterface
 {
     /**
@@ -17,10 +12,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('riverline_worker');
-
-        $rootNode
+        $treeBuilder = new TreeBuilder('riverline_worker');
+        $treeBuilder->getRootNode()
             ->children()
                 ->arrayNode('providers')
                     ->useAttributeAsKey('key')
