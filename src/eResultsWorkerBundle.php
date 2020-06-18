04f643bd3173a1008998ca0ce8e25d275f6e4207
Niels Janssen <nielsjanssen@live.nl>
2020-06-18 11:29:29 +0200

    Fixed container naming conflict

--- src/eResultsWorkerBundle.php
+++ src/eResultsWorkerBundle.php
@@ -6,4 +6,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class eResultsWorkerBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return $this->extension ?: ($this->extension = $this->createContainerExtension());
+    }
 }
